afbc064fa9ef16a5fd741ac50d83268d5b5788a0
Gabriel Goes Santos Rios <gabrielgoesrios@gmail.com>
2023-10-31 01:34:52 -0300

    refactor: melhoras no componente ActivityCard para seguir padroes

--- src/components/Dashboard/Activities/ActivityCard.jsx
+++ src/components/Dashboard/Activities/ActivityCard.jsx
@@ -13,10 +13,10 @@ export default function ActivityCard({ activity: { id, name, startsAt, endsAt, r
   const [participantFront, setParticipantFront] = useState(false);
 
   const isUserParticipating = Participants.some(({ userId }) => userId === user.id);
-  async function postParticipant(activityId, token) {
+  async function postParticipant() {
     if (isUserParticipating || remainingVacancies === 0) return;
     try {
-      await axios.post(`${import.meta.env.VITE_API_URL}/activities/participant`, {activityId}, {headers: { Authorization: `Bearer ${token}` }});
+      await axios.post(`${import.meta.env.VITE_API_URL}/activities/participant`, { activityId: id }, { headers: { Authorization: `Bearer ${token}` } });
       setParticipantFront(true);
       setReloadActivities(previous => previous + 1);
       toast('succesfully subscribed to this activity');
@@ -29,12 +29,12 @@ export default function ActivityCard({ activity: { id, name, startsAt, endsAt, r
   const startHour = new Date(startsAt).getHours();
   const endHour = new Date(endsAt).getHours();
   return (
-    <StyledActivityCard size={endHour - startHour} remainingVacancies={remainingVacancies} color={(participantFront || isUserParticipating) ?'#D0FFDB':'#F1F1F1'}>
+    <StyledActivityCard size={endHour - startHour} remainingVacancies={remainingVacancies} participating={participantFront || isUserParticipating}>
       <div>
         <p>{name}</p>
         <span>{startHour + ':' + new Date(startsAt).getMinutes() + ' - ' + endHour + ':' + new Date(endsAt).getMinutes()}</span>
       </div >
-      <div onClick={remainingVacancies !== 0 ? () => postParticipant(id, token) : ''}>
+      <div onClick={postParticipant}>
         {participantFront || isUserParticipating ? <Check /> : remainingVacancies === 0 ? <Full /> : <Door/>}
         {participantFront || isUserParticipating ? <p>inscrito</p> : remainingVacancies === 0 ? <p>Esgotado</p> : <p>{remainingVacancies} Vaga{remainingVacancies > 1 ? 's' : ''}</p>}
       </div>
@@ -47,7 +47,7 @@ const StyledActivityCard = styled.div`
   display: flex;
   min-height: ${({ size }) => `${size*80}px`};
   border-radius: 5px;
-  background-color: ${({ color }) => color};;
+  background-color: ${({ participating }) => participating ? '#D0FFDB' : '#F1F1F1'};;
   position: relative;
   div:nth-child(1){
     p{
@@ -85,21 +85,21 @@ const StyledActivityCard = styled.div`
       font-size: 12px;
       font-weight: 400;
       line-height: 11px;
-      color: ${({ remainingVacancies }) => remainingVacancies === 0 ? '#CC6666' : '#078632'}
+      color: ${({ remainingVacancies, participating }) => (remainingVacancies > 0 || participating) ? '#078632' : '#CC6666'}
     }
   }
 `;
 
-export const Door = styled(BiExit)`
+const Door = styled(BiExit)`
   color: #078632;
   font-size: 25px;
 `
 
-export const Check = styled(CiCircleCheck)`
+const Check = styled(CiCircleCheck)`
   color: #078632;
   font-size: 25px;
 `
-export const Full = styled(CiCircleRemove)`
+const Full = styled(CiCircleRemove)`
   color: #CC6666;
   font-size: 25px;
 `
